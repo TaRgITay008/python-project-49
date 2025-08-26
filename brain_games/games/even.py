e40ba0f23205359b7b6e8e56b01cc16b9edaa9b1
София <sofia@MacBook-Air-Sofia.local>
2025-08-26 18:48:41 +0500

    add .

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -9,7 +9,7 @@ def play_game():
     import prompt
     name = prompt.string('May I have your name?')
     print(f'Hello, {name}!')
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
     correct_answers = 0
     while correct_answers < 3:
